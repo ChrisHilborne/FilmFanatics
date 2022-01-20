ce29a5036ae35ba01166daf316bb5c22912ed40b
ChrisHilborne <chris.hilborne@gmail.com>
2022-01-20 13:50:11 +0100

    First Commit

---
+++ src/main/java/io/chilborne/filmfanatic/FilmFanaticApplication.java
@@ -0,0 +1,13 @@
+package io.chilborne.filmfanatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmFanaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmFanaticApplication.class, args);
+	}
+
+}
