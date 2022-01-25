510b09891329479fdf2fcbe775b0e0d3c63a9bf2
ChrisHilborne <chris.hilborne@gmail.com>
2022-01-25 13:46:37 +0100

    Created FilmRepository with query methods as well as test class

--- src/main/java/io/chilborne/filmfanatic/repository/FilmRepository.java
+++ src/main/java/io/chilborne/filmfanatic/repository/FilmRepository.java
@@ -3,16 +3,23 @@ package io.chilborne.filmfanatic.repository;
 import io.chilborne.filmfanatic.domain.Film;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.Set;
 
 public interface FilmRepository extends CrudRepository<Film, Long> {
 
-  Optional<Film> findByTitle(String name);
+  Optional<Film> findByTitle(String title);
   Set<Film> findByYear(int year);
+  Set<Film> findByYearGreaterThan(int year);
+  Set<Film> findByYearLessThan(int year);
+  Set<Film> findByYearBetween(int firstYear, int lastYear);
   Set<Film> findByDurationLessThan(int duration);
 
-  @Query("SELECT from Film f WHERE AVG(f.scores) > ?1")
+  Set<Film> findByDirectorNameAndDirectorSurname(@Param("director.name") String name, @Param("director.surname") String surname);
+  Set<Film> findByActorsNameAndActorsSurname(@Param("actors.name") String name,@Param("actors.surname") String surname);
+
+  @Query(value = "SELECT * FROM films f HAVING AVG(f.scores) >= :score", nativeQuery = true)
   Set<Film> findFilmByAverageScoreGreaterThan(int score);
 }
