385f0a47bd1151a9399faaafa426a1c804ed975b
ChrisHilborne <chris.hilborne@gmail.com>
2022-01-27 13:28:00 +0100

    Created RoleRepository

---
+++ src/main/java/io/chilborne/filmfanatic/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package io.chilborne.filmfanatic.repository;
+
+import io.chilborne.filmfanatic.domain.Role;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends CrudRepository<Role, Long> {
+
+  Optional<Role> findByNameIgnoreCase(String name);
+}
