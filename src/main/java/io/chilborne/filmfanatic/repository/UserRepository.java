560bf80cad71759a2cd0076cf87a2a94de1242a6
ChrisHilborne <chris.hilborne@gmail.com>
2022-01-27 13:23:40 +0100

    Added query method to find user's by "active" property

--- src/main/java/io/chilborne/filmfanatic/repository/UserRepository.java
+++ src/main/java/io/chilborne/filmfanatic/repository/UserRepository.java
@@ -4,9 +4,11 @@ import io.chilborne.filmfanatic.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
+import java.util.Set;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
   Optional<User> findByUsername();
+  Set<User> findByActive(boolean active);
 
 }
