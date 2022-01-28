a2ee1e97b60f27b424890019206c4147e65c6b92
ChrisHilborne <chris.hilborne@gmail.com>
2022-01-28 15:26:00 +0100

    Changed method annotation for index()
    Moved back to /resources/
    
    removed th from <a .. th:href="/" .. >

--- src/main/java/io/chilborne/filmfanatic/controller/WebController.java
+++ src/main/java/io/chilborne/filmfanatic/controller/WebController.java
@@ -4,16 +4,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class WebController {
 
   private final Logger logger = LoggerFactory.getLogger(WebController.class);
 
-  @GetMapping("/")
-  @PostMapping("/")
+  @RequestMapping(path = "/*", method = {RequestMethod.GET, RequestMethod.POST})
   public String index(Model model) {
     logger.info("Connection... serving index.html");
     return "index";
