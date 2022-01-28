4c056558fdad259bd94bfabc4fb9c47713fe5b3d
ChrisHilborne <chris.hilborne@gmail.com>
2022-01-28 12:16:12 +0100

    Created basic WebSecurityConfig class

---
+++ src/main/java/io/chilborne/filmfanatic/security/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package io.chilborne.filmfanatic.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private final Logger logger = LoggerFactory.getLogger(WebSecurityConfig.class);
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.
+      authorizeRequests()
+      .antMatchers("/").permitAll()
+      .antMatchers("/error").permitAll();
+  }
+}
