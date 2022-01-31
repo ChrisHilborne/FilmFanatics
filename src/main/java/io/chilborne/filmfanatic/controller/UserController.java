4a58c8ad41b9e4b5126950b805d2fc88b6f3bc0c
ChrisHilborne <chris.hilborne@gmail.com>
2022-01-31 14:09:44 +0100

    Added methods to respond to GET requests for User profile and Post Requests to create new user

--- src/main/java/io/chilborne/filmfanatic/controller/UserController.java
+++ src/main/java/io/chilborne/filmfanatic/controller/UserController.java
@@ -1,7 +1,42 @@
 package io.chilborne.filmfanatic.controller;
 
+import io.chilborne.filmfanatic.domain.User;
+import io.chilborne.filmfanatic.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class UserController {
+
+  private final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+  private final UserService userService;
+
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @GetMapping("/login")
+  public String login(Model model) {
+    return "login";
+  }
+
+  @GetMapping("/registration")
+  public String registerUser(Model model) {
+    model.addAttribute("user", new User());
+    return "registration";
+  }
+
+  @GetMapping("/profile/{username}")
+  public String profile(Model model, HttpServletRequest request, @PathVariable String username) {
+    model.addAttribute("user", userService.getUser(username));
+    return "profile";
+  }
 }
