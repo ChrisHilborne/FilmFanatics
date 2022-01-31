a7482dcc2651fe74fecaddc2870e0f448c3d7d8c
ChrisHilborne <chris.hilborne@gmail.com>
2022-01-31 13:55:45 +0100

    Configured Password Encoder and ignore security checks for static content

--- src/main/java/io/chilborne/filmfanatic/security/WebSecurityConfig.java
+++ src/main/java/io/chilborne/filmfanatic/security/WebSecurityConfig.java
@@ -2,10 +2,15 @@ package io.chilborne.filmfanatic.security;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -25,5 +30,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
       // TODO remove before release
       .csrf().disable()
       .headers().frameOptions().disable();
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().antMatchers(
+      "/resources/**",
+      "/static/**",
+      "/templates/**",
+      "/images/**",
+      "/css/**",
+      "/js/**",
+      "/fonts/**",
+      "/webjars/**"
+    );
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
