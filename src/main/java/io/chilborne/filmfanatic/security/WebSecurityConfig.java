bf5ef17433d2166d4e567387fc045f9e9ac0faec
ChrisHilborne <chris.hilborne@gmail.com>
2022-01-31 14:10:15 +0100

    Import Sanitization

--- src/main/java/io/chilborne/filmfanatic/security/WebSecurityConfig.java
+++ src/main/java/io/chilborne/filmfanatic/security/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
