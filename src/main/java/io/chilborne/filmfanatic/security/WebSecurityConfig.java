e9b9dd70e9219c0a4489b489c6c56f5b6942adcb
ChrisHilborne <chris.hilborne@gmail.com>
2022-01-31 12:40:07 +0100

    Allow access to h2-console

--- src/main/java/io/chilborne/filmfanatic/security/WebSecurityConfig.java
+++ src/main/java/io/chilborne/filmfanatic/security/WebSecurityConfig.java
@@ -3,7 +3,6 @@ package io.chilborne.filmfanatic.security;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,6 +18,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     http.
       authorizeRequests()
       .antMatchers("/").permitAll()
-      .antMatchers("/error").permitAll();
+      .antMatchers("/error").permitAll()
+      .antMatchers("/h2-console/*").permitAll()
+    .and()
+      // in order to allow access to h2-console for testing
+      //TODO remove before release
+      .csrf().disable()
+      .headers().frameOptions().disable();
   }
 }
