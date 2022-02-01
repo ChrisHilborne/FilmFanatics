55dfa41a089aa9cb374bdb4d4d090c924c43427e
ChrisHilborne <chris.hilborne@gmail.com>
2022-02-01 12:16:33 +0100

    Configured WebSecurity for Login and Logout

--- src/main/java/io/chilborne/filmfanatic/security/WebSecurityConfig.java
+++ src/main/java/io/chilborne/filmfanatic/security/WebSecurityConfig.java
@@ -4,30 +4,59 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static io.chilborne.filmfanatic.util.Constants.*;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   private final Logger logger = LoggerFactory.getLogger(WebSecurityConfig.class);
+  private final UserDetailsServiceImpl userDetailsService;
+
+  public WebSecurityConfig(UserDetailsServiceImpl userDetailsService) {
+    this.userDetailsService = userDetailsService;
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService)
+      .passwordEncoder(passwordEncoder());
+  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.
       authorizeRequests()
-      .antMatchers("/").permitAll()
-      .antMatchers("/error").permitAll()
-      .antMatchers("/h2-console/*").permitAll()
+        .antMatchers("/").permitAll()
+        .antMatchers("/register").permitAll()
+        .antMatchers("/login").permitAll()
+        .antMatchers("/film/**").permitAll()
+        .antMatchers("/films/**").permitAll()
+        .antMatchers("/h2-console/*").permitAll()
+        .antMatchers("/admin/**").hasAnyAuthority(ADMIN_ROLE)
+      .anyRequest().authenticated()
     .and()
+      .formLogin()
+        .loginPage(LOGIN_URL)
+        .defaultSuccessUrl(LOGIN_SUCCESS_URL)
+        .failureUrl(LOGIN_FAILURE)
+    .and()
+      .logout()
+        .logoutRequestMatcher(new AntPathRequestMatcher(LOGOUT_URL))
+        .logoutSuccessUrl(LOGOUT_SUCCESS_URL)
+    .and()
+      .csrf().disable()
       // in order to allow access to h2-console for testing
       // TODO remove before release
-      .csrf().disable()
       .headers().frameOptions().disable();
   }
 
