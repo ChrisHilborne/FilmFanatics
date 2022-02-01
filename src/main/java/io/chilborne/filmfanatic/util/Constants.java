6a7175a9873dee2b95ae9630bd898c7ba3eef3e3
ChrisHilborne <chris.hilborne@gmail.com>
2022-02-01 12:10:01 +0100

    Created security constants

---
+++ src/main/java/io/chilborne/filmfanatic/util/Constants.java
@@ -0,0 +1,16 @@
+package io.chilborne.filmfanatic.util;
+
+public class Constants {
+
+  public static final String USER_ROLE = "user";
+  public static final String ADMIN_ROLE = "admin";
+
+  public static final String LOGIN_FAILURE = "/login?error=true";
+  public static final String LOGIN_URL = "/login";
+  public static final String LOGIN_SUCCESS_URL = "/";
+
+  public static final String LOGOUT_URL = "/logout";
+  public static final String LOGOUT_SUCCESS_URL = "/?message=logout";
+  public static final String JSESSIONID = "JSESSIONID";
+
+}
