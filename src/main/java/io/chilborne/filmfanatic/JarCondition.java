f8d5acace0e094a92b3fd3922ab6492f51c718f4
ChrisHilborne <chris.hilborne@gmail.com>
2022-04-21 11:22:43 +0200

    Removed println lines for debugging from .jar

--- src/main/java/io/chilborne/filmfanatic/JarCondition.java
+++ src/main/java/io/chilborne/filmfanatic/JarCondition.java
@@ -8,22 +8,10 @@ import org.springframework.core.type.AnnotatedTypeMetadata;
 public class JarCondition implements Condition {
   @Override
   public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-    ApplicationArguments args = context.getBeanFactory().getBean(ApplicationArguments.class);
-
-    System.out.println("Option names:" + args.getOptionNames());
-    for (String name : args.getOptionNames()) {
-      System.out.println("Option: " + name + "::" + args.getOptionValues(name));
-    }
-
-    System.out.println("NonOptionArgs: " + args.getNonOptionArgs());
-
-    boolean matches = context.getBeanFactory()
+    return context.getBeanFactory()
       .getBean(ApplicationArguments.class)
       .getOptionNames()
       .contains("jar");
-
-    System.out.println("matches:" + matches);
-    return matches;
   }
 }
 
